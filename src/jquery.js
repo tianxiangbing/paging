5d74bd7600f29988e3d045051c8a511f33252aa8
tianxiangbing <55342775@qq.com>
2017-02-14 09:02:09 +0800

    'jquery'

--- src/jquery.js
+++ src/jquery.js
@@ -13,11 +13,7 @@
  */
 
 (function( global, factory ) {
-if (typeof define === 'function' && define.amd) {
-    define([], function(){
-    		return factory(global)
-    });
-  } else 
+
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 		// For CommonJS and CommonJS-like environments where a proper window is present,
 		// execute the factory and get jQuery
